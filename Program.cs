b2bbc9b6c92d10efb9220f433e2e66f9678f53b5
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-16 23:36:17 +0200

    removed some test code.

--- Program.cs
+++ Program.cs
@@ -1,60 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata.Ecma335;
 using bf_bot;
-using bf_bot.Json;
-using bf_bot.TO;
+using bf_bot.Strategies.Soccer;
 using Microsoft.Extensions.Logging;
-// using Microsoft.Extensions.Logging.Console;
 
-// create initializer obj, where are stored all the necessary things to create the client.
-var initializer = Utility.CreateInitializer();
 
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
     builder
-        .AddFilter("Program", LogLevel.Warning)
+        .SetMinimumLevel(LogLevel.Trace)
+        // .AddFilter("Default", LogLevel.Trace)
         .AddSimpleConsole( options => {
             options.IncludeScopes = true;
             options.SingleLine = true;
             options.TimestampFormat = "dd/mm/yyyy hh:mm:ss ";
         });
 });
-
 ILogger logger = loggerFactory.CreateLogger<Program>();
-logger.LogTrace("Example trace message");
-logger.LogDebug("Example debug message");
-logger.LogInformation("Example info message");
-logger.LogWarning("Example warn message");
-logger.LogError("Example error message");
-logger.LogCritical("Example critical message");
 
+var initializer = Utility.CreateInitializer();
 
-return;
+var client = new BetfairClient(initializer, logger);
 
-var client = new BetfairClient(initializer);
+var bothTeamToScoreStrategy = new BothTeamToScore(client, logger);
 
-var loginResult = await client.Login();
+await bothTeamToScoreStrategy.Start();
 
-if(loginResult.IsOk)
-{
-    Console.WriteLine("Successfully logged in.");
-    // Console.WriteLine("Response details: ");
-    // Console.WriteLine(Utility.PrettyJsonObject(loginResult));
-    Console.WriteLine("\n\nAuth Token: " + client.AuthToken);
-}
-else
-{
-    Console.WriteLine("Error when logging in.");
-    Console.WriteLine(Utility.PrettyJsonObject(loginResult));
-    throw new System.Exception("Cannot authenticate.");
-}
-
-var marketFilter = new MarketFilter();
-
-var eventTypes = await client.listEventTypes(marketFilter);
-
-ISet<string> eventypeIds = new HashSet<string>();   
-foreach (EventTypeResult eventType in eventTypes)
-{
-    Console.WriteLine("\nEventType: " + JsonConvert.Serialize<EventTypeResult>(eventType));
-}
+logger.LogInformation("Program terminated.");
