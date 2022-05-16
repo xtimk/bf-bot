636d09a1f54fbe48966aa34e86e7e4d0b78ab1e2
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-16 16:34:30 +0200

    feat: adding various constants.

---
+++ Constants/BetfairConstants.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bf_bot.Constants
+{
+    public static class BetfairConstants
+    {
+        public static readonly string LIST_EVENT_TYPES_METHOD = "listEventTypes";
+        public static readonly string LIST_MARKET_CATALOGUE_METHOD = "listMarketCatalogue";
+        public static readonly string LIST_MARKET_TYPES_METHOD = "listMarketTypes";
+        public static readonly string LIST_MARKET_BOOK_METHOD = "listMarketBook";
+        public static readonly string PLACE_ORDERS_METHOD = "placeOrders";
+        public static readonly string LIST_MARKET_PROFIT_AND_LOST_METHOD = "listMarketProfitAndLoss";
+        public static readonly string LIST_CURRENT_ORDERS_METHOD = "listCurrentOrders";
+        public static readonly string LIST_CLEARED_ORDERS_METHOD = "listClearedOrders";
+        public static readonly string CANCEL_ORDERS_METHOD = "cancelOrders";
+        public static readonly string REPLACE_ORDERS_METHOD = "replaceOrders";
+        public static readonly string UPDATE_ORDERS_METHOD = "updateOrders";
+        public const string FILTER = "filter";
+        public static readonly string LOCALE = "locale";
+        public static readonly String CURRENCY_CODE = "currencyCode";
+        public static readonly String MARKET_PROJECTION = "marketProjection";
+        public static readonly String MATCH_PROJECTION = "matchProjection";
+        public static readonly String ORDER_PROJECTION = "orderProjection";
+        public static readonly String PRICE_PROJECTION = "priceProjection";
+        public static readonly String SORT = "sort";
+        public static readonly String MAX_RESULTS = "maxResults";
+        public static readonly String MARKET_IDS = "marketIds";
+        public static readonly String MARKET_ID = "marketId";
+        public static readonly String INSTRUCTIONS = "instructions";
+        public static readonly String CUSTOMER_REFERENCE = "customerRef";
+        public static readonly string INCLUDE_SETTLED_BETS = "includeSettledBets";
+        public static readonly String INCLUDE_BSP_BETS = "includeBspBets";
+        public static readonly String NET_OF_COMMISSION = "netOfCommission";
+        public static readonly String BET_IDS = "betIds";
+        public static readonly String PLACED_DATE_RANGE = "placedDateRange";
+        public static readonly String ORDER_BY = "orderBy";
+        public static readonly String SORT_DIR = "sortDir";
+        public static readonly String FROM_RECORD = "fromRecord";
+        public static readonly String RECORD_COUNT = "recordCount";
+        public static readonly string BET_STATUS = "betStatus";
+        public static readonly string EVENT_TYPE_IDS = "eventTypeIds";
+        public static readonly string EVENT_IDS = "eventIds";
+        public static readonly string RUNNER_IDS = "runnerIds";
+        public static readonly string SIDE = "side";
+        public static readonly string SETTLED_DATE_RANGE = "settledDateRange";
+        public static readonly string GROUP_BY = "groupBy";
+        public static readonly string INCLUDE_ITEM_DESCRIPTION = "includeItemDescription";
+    }
+}
---
+++ Constants/HeaderConstants.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bf_bot.Constants
+{
+    public static class HeaderConstants
+    {
+        public const string APPKEY_HEADER = "X-Application";
+        public const string SESSION_TOKEN_HEADER = "X-Authentication";
+    }
+}
