167c20bc4cda6cf3c3717f76f6792f2abdaf48de
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-16 17:55:51 +0200

    feat: started to add logging extension.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
 using bf_bot;
 using bf_bot.Json;
 using bf_bot.TO;
+using Microsoft.Extensions.Logging;
+// using Microsoft.Extensions.Logging.Console;
 
 // create initializer obj, where are stored all the necessary things to create the client.
 var initializer = Utility.CreateInitializer();
 
-BetfairClient client = new BetfairClient(initializer);
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddFilter("Program", LogLevel.Warning)
+        .AddSimpleConsole( options => {
+            options.IncludeScopes = true;
+            options.SingleLine = true;
+            options.TimestampFormat = "dd/mm/yyyy hh:mm:ss ";
+        });
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+logger.LogTrace("Example trace message");
+logger.LogDebug("Example debug message");
+logger.LogInformation("Example info message");
+logger.LogWarning("Example warn message");
+logger.LogError("Example error message");
+logger.LogCritical("Example critical message");
+
+
+return;
+
+var client = new BetfairClient(initializer);
 
 var loginResult = await client.Login();
 
@@ -33,4 +58,3 @@ foreach (EventTypeResult eventType in eventTypes)
 {
     Console.WriteLine("\nEventType: " + JsonConvert.Serialize<EventTypeResult>(eventType));
 }
-
