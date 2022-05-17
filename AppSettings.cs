99ad88736a8befac499a2e29a8107b21c2e249c2
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-17 23:36:01 +0200

    clean up unused imports.

--- AppSettings.cs
+++ AppSettings.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using System.IO;
 // using Microsoft.Extensions.Configuration.Binder;
 
 namespace bf_bot
