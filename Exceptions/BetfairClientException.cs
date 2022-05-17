9e1eb0ad6fe7c5b6d3d2e4091f809e104e34eff7
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-17 11:45:01 +0200

    feat: added custom exception class.

---
+++ Exceptions/BetfairClientException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace bf_bot.Exceptions
+{
+    public class BetfairClientException : Exception
+    {
+        public BetfairClientException()
+        {
+        }
+
+        public BetfairClientException(ILogger logger, string message) : base(message)
+        {
+            logger.LogError("Exception encountered: " + message);
+        }
+
+        public BetfairClientException(ILogger logger, string message, Exception inner) : base(message, inner)
+        {
+            logger.LogError("Exception encountered: " + message + ". Inner exception: " + inner);
+        }
+    }
+}
