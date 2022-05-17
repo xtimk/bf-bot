22da5b73c6a3bda04a84f946d0193ba00a37eae5
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-17 11:45:31 +0200

    style: formatting code

--- TO/MarketSort.cs
+++ TO/MarketSort.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace bf_bot.TO
 {
-     [JsonConverter(typeof(StringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum MarketSort
     {
          MINIMUM_TRADED, MAXIMUM_TRADED, MINIMUM_AVAILABLE, MAXIMUM_AVAILABLE, FIRST_TO_START, LAST_TO_START,
