99377e05d79b107447877fa240815dfc0429a1ae
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-19 19:56:34 +0200

    style: cleanup.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 var serviceProvider = new ServiceCollection().AddLogging(builder => {
     builder
-        .SetMinimumLevel(LogLevel.Debug)
+        .SetMinimumLevel(LogLevel.Information)
         .AddSimpleConsole( options => {
             options.IncludeScopes = true;
             options.SingleLine = true;
@@ -17,24 +17,6 @@ var serviceProvider = new ServiceCollection().AddLogging(builder => {
     .AddScoped<IClient, BetfairClient>(provider => new BetfairClient(provider.GetRequiredService<ILoggerFactory>(), Utility.CreateInitializer()))
     .AddScoped<IStrategy, BothTeamToScore>(provider => new BothTeamToScore(provider.GetRequiredService<IClient>(), provider.GetRequiredService<ILoggerFactory>()))
     .BuildServiceProvider();
-    
-// serviceProvider.GetService<ILoggerFactory>();
-
-// var bfClient = serviceProvider.GetService<IClient>();
-
-// serviceProvider.GetService<IClient>().Init(Utility.CreateInitializer());
-
-// var initializer = Utility.CreateInitializer();
-// bfClient.Init(initializer);
 
 var btScoreStrategy = serviceProvider.GetService<IStrategy>();
 await btScoreStrategy.Start();
-
-
-// var client = new BetfairClient(initializer, loggerFactory);
-
-// var bothTeamToScoreStrategy = new BothTeamToScore(client, logger);
-
-// await bothTeamToScoreStrategy.Start();
-
-// logger.LogInformation("Program terminated.");
