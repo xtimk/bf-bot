9c97cc0a96bde88c026fcb60ac79ca9c45961fc8
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-22 13:03:21 +0200

    feat: add necessary func in iwallet interface.

--- Wallets/IWallet.cs
+++ Wallets/IWallet.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,25 @@ namespace bf_bot.Wallets
 {
     public interface IWallet
     {
-        
+        /// <summary>
+        /// Given the price returns the amount to bet
+        /// </summary>
+        double getAmountToBet(double price);
+
+        /// <summary>
+        /// Gets the actual balance of the wallet
+        /// </summary>
+        double getBalance();
+
+        /// <summary>
+        /// Signals the wallet that a bet has been placed
+        /// </summary>
+        void signalPlaceBet(double amount);
+
+        /// <summary>
+        /// Signals the wallet that a bet was won
+        /// </summary>
+        void signalWin(double amount);
+
     }
 }
