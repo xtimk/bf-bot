218f195f51130f673550d3c5d94a7ca1a23a620d
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-22 10:19:18 +0200

    feat: add wallet skeleton.

---
+++ Wallets/Impl/SimpleProgressionWallet.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bf_bot.Wallets.Impl
+{
+    public class SimpleProgressionWallet : IWallet
+    {
+        
+    }
+}
