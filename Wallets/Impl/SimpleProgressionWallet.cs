8ee5ad24822cad7fe295a63202401dcbdde55ddf
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-22 13:03:42 +0200

    feat: impl simple progression wallet.

--- Wallets/Impl/SimpleProgressionWallet.cs
+++ Wallets/Impl/SimpleProgressionWallet.cs
@@ -1,12 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace bf_bot.Wallets.Impl
 {
     public class SimpleProgressionWallet : IWallet
     {
+        private double _balance;
         
+        // trace step just for printing purposes, it is really not necessary for the logic.
+        private int _step;
+        private readonly double _win_per_cycle;
+        private double _desired_wallet_balance;
+
+        public SimpleProgressionWallet(double balance, double win_per_cycle)
+        {
+            _win_per_cycle = win_per_cycle;
+            _balance = balance;
+            _desired_wallet_balance = _balance + _win_per_cycle;
+        }
+
+        public double getAmountToBet(double price)
+        {
+            var amountToWin = _desired_wallet_balance - _balance;
+            return amountToWin/(price - 1);
+        }
+
+        public void signalPlaceBet(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public void signalWin(double amount)
+        {
+            _balance += amount;
+            _desired_wallet_balance = _balance + _win_per_cycle;
+            _step = 1;
+        }
+
+        public double getBalance()
+        {
+            return _balance;
+        }
     }
 }
