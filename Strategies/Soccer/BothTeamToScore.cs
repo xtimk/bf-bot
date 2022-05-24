cb1b20ed11d0b08d9486e1c2c13256863db868b4
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-24 18:58:49 +0200

    add IWallet, add placeRealBet, placeFakeBet.

--- Strategies/Soccer/BothTeamToScore.cs
+++ Strategies/Soccer/BothTeamToScore.cs
@@ -2,6 +2,7 @@ using bf_bot.Constants;
 using bf_bot.Exceptions;
 using bf_bot.Json;
 using bf_bot.TO;
+using bf_bot.Wallets;
 using Microsoft.Extensions.Logging;
 
 namespace bf_bot.Strategies.Soccer
@@ -10,7 +11,7 @@ namespace bf_bot.Strategies.Soccer
     {
         private string _marketCatalogueMaxResults = "100";
         private MarketBookFilterCondition _condition = new() {
-            MaxPrice = 3,
+            MaxPrice = 4,
             MinPrice = 1.7,
             MinSize = 10
         };
@@ -20,8 +21,10 @@ namespace bf_bot.Strategies.Soccer
         private readonly ILogger<BothTeamToScore> _logger;
         private bool _active = false;
         private readonly RunningMode _mode;
-        public BothTeamToScore(IClient client, ILoggerFactory loggerFactory, RunningMode mode)
+        private readonly IWallet _wallet;
+        public BothTeamToScore(RunningMode mode, IClient client, ILoggerFactory loggerFactory, IWallet wallet)
         {
+            _wallet = wallet;
             _mode = mode;
             _logger = loggerFactory.CreateLogger<BothTeamToScore>();
             _client = client;
@@ -41,6 +44,9 @@ namespace bf_bot.Strategies.Soccer
             }
 
             _active = true;
+
+            // init wallet... ToDo: check if we can init in a better way..
+            _wallet.Init(1000, 2);
 
             try
             {
@@ -92,7 +98,110 @@ namespace bf_bot.Strategies.Soccer
                 }
 
                 var marketBookToBet = SelectOneMarketBook(marketBooks);
+
+                var betPlaced = await PlaceBet(marketBookToBet);
+                if(!betPlaced)
+                    continue;
+
+                await WaitForBetResult(marketBookToBet);
+
+                // just stop at the first iteration for debugging logs after..
+                _active = false;
             }
+        }
+
+        public async Task<bool> WaitForBetResult(MarketBook marketBook)
+        {
+            ISet<PriceData> priceData = new HashSet<PriceData>();
+            priceData.Add(PriceData.EX_BEST_OFFERS);
+            
+            var priceProjection = new PriceProjection();
+            priceProjection.PriceData = priceData;
+
+            var marketId = marketBook.MarketId;
+            var selectionId = marketBook.Runners[0].SelectionId;
+
+            var runnerBook = await _client.listRunnerBook(marketId, selectionId, priceProjection);
+            _logger.LogTrace(JsonConvert.Serialize<IList<MarketBook>>(runnerBook));
+
+            while(runnerBook.First().Runners[0].Status != RunnerStatus.WINNER && runnerBook.First().Runners[0].Status != RunnerStatus.LOSER)
+            {
+                Thread.Sleep(2000);
+                runnerBook = await _client.listRunnerBook(marketId, selectionId, priceProjection);
+                _logger.LogTrace(JsonConvert.Serialize<IList<MarketBook>>(runnerBook));
+            }
+
+            if(runnerBook.First().Runners[0].Status == RunnerStatus.WINNER)
+            {
+                _logger.LogInformation("Bet winned!");
+                _wallet.signalWin();
+                return true;
+            }
+            else
+            {
+                _logger.LogInformation("Bet is lost.");
+                _wallet.signalLose();
+                return false;
+            }
+        }
+        public async Task<bool> PlaceBet(MarketBook marketBook)
+        {
+
+            // Runners[0] stays for BothTeamToScore YES
+            // I take first() since im using EX_BEST_OFFERS, that returns a single offer.
+            var price = marketBook.Runners[0].ExchangePrices.AvailableToBack.First().Price;
+            var size = marketBook.Runners[0].ExchangePrices.AvailableToBack.First().Size;
+
+            var marketId = marketBook.MarketId;
+            var selectionId = marketBook.Runners[0].SelectionId;
+
+            var amountToBet = Math.Round(_wallet.getAmountToBet(price), 2, MidpointRounding.AwayFromZero);
+            if(amountToBet > size)
+            {
+                _logger.LogInformation("Insufficient liquidity in the bet pool.");
+                return false;
+            }
+            if(amountToBet < 2)
+            {
+                _logger.LogWarning("The betting amount <" + amountToBet + "> is less than the minimal bet (2 EUR). Will automatically bet 2 EUR.");
+                amountToBet = 2;
+            }
+
+            var betPlaced = false;
+            if(_mode == RunningMode.REAL)
+            {
+                betPlaced = await PlaceRealBet(marketId, selectionId, amountToBet, price);
+            }
+            else
+            {
+                betPlaced = PlaceFakeBet(marketId, selectionId, amountToBet, price);
+            }
+
+            if(!betPlaced)
+            {
+                _logger.LogWarning("Bet not placed!");
+                return false;
+            }
+
+            _wallet.signalPlaceBet(amountToBet);
+
+            _logger.LogInformation("Placed bet " + amountToBet + "EUR @ " + price);
+            _logger.LogInformation("Betfair link: https://www.betfair.it/exchange/plus/football/market/" + marketId);
+
+            return true;
+        }
+
+#pragma warning disable
+        private async Task<bool> PlaceRealBet(string marketId, long selectionId, double amountToBet, double price)
+        {
+            _logger.LogCritical("Place real bet here. Not implemented yet!");
+            return false;
+        }
+
+        private bool PlaceFakeBet(string marketId, long selectionId, double amountToBet, double price)
+        {
+            _logger.LogInformation("Placed bet in TEST mode.");
+            return true;
         }
 
         public MarketBook SelectOneMarketBook(List<MarketBook> marketBooks)
@@ -182,8 +291,6 @@ namespace bf_bot.Strategies.Soccer
         }
         public async Task<List<MarketBook>> GetMarketBooks(List<MarketCatalogue> marketCatalogues)
         {
-            var listToReturn = new List<string>();
-
             ISet<PriceData> priceData = new HashSet<PriceData>();
             priceData.Add(PriceData.EX_BEST_OFFERS);
             
