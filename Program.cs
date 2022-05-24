0329576b7ba319ba327c2cf0b4fba78e2a7b764a
Tiziano Mele <tiziano.mele09@gmail.com>
2022-05-24 18:57:23 +0200

    feat: add IWallet integration with IStrategy.

--- Program.cs
+++ Program.cs
@@ -4,21 +4,31 @@ using bf_bot.Strategies.Soccer;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using bf_bot.Constants;
+using bf_bot.Wallets;
+using bf_bot.Wallets.Impl;
 
 var runningMode = RunningMode.TEST;
 
 var serviceProvider = new ServiceCollection().AddLogging(builder => {
     builder
-        .SetMinimumLevel(LogLevel.Information)
+        .SetMinimumLevel(LogLevel.Trace)
         .AddSimpleConsole( options => {
             options.IncludeScopes = true;
             options.SingleLine = true;
             options.TimestampFormat = "[dd/mm/yyyy hh:mm:ss] ";
         });
     })
+    .AddScoped<IWallet, SimpleProgressionWallet>()
     .AddSingleton<BetfairClientInitializer>()
     .AddScoped<IClient, BetfairRestClient>(provider => new BetfairRestClient(provider.GetRequiredService<ILoggerFactory>(), Utility.CreateInitializer()))
-    .AddScoped<IStrategy, BothTeamToScore>(provider => new BothTeamToScore(provider.GetRequiredService<IClient>(), provider.GetRequiredService<ILoggerFactory>(), runningMode))
+    .AddScoped<IStrategy, BothTeamToScore>(provider => 
+        new BothTeamToScore(
+                runningMode, 
+                provider.GetRequiredService<IClient>(), 
+                provider.GetRequiredService<ILoggerFactory>(), 
+                provider.GetRequiredService<IWallet>()
+            )
+        )
     .BuildServiceProvider();
 
 var btScoreStrategy = serviceProvider.GetService<IStrategy>();
